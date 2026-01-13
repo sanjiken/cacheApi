853562b90faa063cd314d13fb3eb2906a72ba174
nader <touibi.nader@gmail.com>
2026-01-13 10:00:59 +0100

    first commit
    first commit

---
+++ src/main/java/com/artere/cacheApi/CacheApiApplication.java
@@ -0,0 +1,13 @@
+package com.artere.cacheApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artere/cacheApi/CacheApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artere.cacheApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
